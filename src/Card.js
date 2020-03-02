8941551c3fb2e393d4f37e83cc16c5c90c4bfefc
LeninJaramillo <lenin_jaramillo@hotmail.com>
2020-03-02 14:58:44 -0500

    flip function

---
+++ src/Card.js
@@ -0,0 +1,17 @@
+import React from 'react';
+const back = './back.png';
+
+export default function Card({face, clicker, status}){
+    let card;
+        if(status === 'up'){
+            card = <img className='sizecards' src={face} id='c1' onClick={() => clicker()}/>
+
+
+        } else {
+            card = <img className='sizecards' src={back} id='back' onClick={() => clicker()}/>
+        }
+    return(
+        <div>{card}</div>
+    )
+}
+
