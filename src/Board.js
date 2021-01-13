962dd17fc615e40f525067ab65de8704c7b84535
Fernando <lenin_jaramillo@hotmail.com>
2021-01-13 23:09:47 +0100

    try git

--- src/Board.js
+++ src/Board.js
@@ -29,7 +29,7 @@ export default function Board(props) {
                 </div>
                 <div className="cardarea">
                     <div className="row1">
-                        <Card status={props.statuses[0]} face={props.faces[0]} clicker={() => props.flipCard(0)} />  
+                        {/* <Card status={props.statuses[0]} face={props.faces[0]} clicker={() => props.flipCard(0)} />  
                         <Card status={props.statuses[1]} face={props.faces[1]} clicker={() => props.flipCard(1)} />
                         <Card status={props.statuses[2]} face={props.faces[2]} clicker={() => props.flipCard(2)} />
                         <Card status={props.statuses[3]} face={props.faces[3]} clicker={() => props.flipCard(3)} />
@@ -41,7 +41,7 @@ export default function Board(props) {
                         <Card status={props.statuses[7]} face={props.faces[7]} clicker={() => props.flipCard(7)} />
                         <Card status={props.statuses[8]} face={props.faces[8]} clicker={() => props.flipCard(8)} />
                         <Card status={props.statuses[9]} face={props.faces[9]} clicker={() => props.flipCard(9)} />
-                    </div>
+                    </div> */}
                 </div>
             </div>
         </div>
